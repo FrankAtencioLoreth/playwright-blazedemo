734c9c3da9c557bf4c2777566c3e5b0f432ab8ef
Frank <frank.atencio.loreth@gmail.com>
2024-08-28 11:39:22 -0500

    configuration for multiple environmets

--- tests/BlazeDemoTest.spec.ts
+++ tests/BlazeDemoTest.spec.ts
@@ -1,14 +1,18 @@
 import { test, expect } from '@playwright/test';
 import { BookingsPage } from '../pages/BookingsPage';
+import Environment from '../utils/Environment';
 
 test.beforeEach( async({ page }) => {
 
-    console.log(`Test starter`);
+    console.log(`Test starter with BASE URL: ${ Environment.BASEURL }`);
+    console.log(`Test starter with USERNAME: ${ Environment.USERNAME }`);
+    console.log(`Test starter with PASSWORD: ${ Environment.PASSWORD }`);
 
 });
 
-test('Test for BlazeDemo', async ({ page }) => {
-    
+test(
+    'Test for BlazeDemo', { tag: '@TC_001' }, async ({ page }) => {
+
     const bookingPage = new BookingsPage(page);
 
     await test.step(`Go to the index page`, async () => {
---
+++ utils/Environment.ts
@@ -0,0 +1,7 @@
+export default class Environment {
+
+    public static BASEURL = process.env.BASE_URL;
+    public static USERNAME = process.env.USER_NAME;
+    public static PASSWORD = process.env.PASSWORD;
+
+}
